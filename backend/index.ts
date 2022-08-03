d385a727bc98610e47c1d2e2c84314fece8bf2e2
Facundo Azcue <fazcue@gmail.com>
2022-08-03 17:49:20 -0300

    Updating routes
    Added login & register endpoint
    Fixed getting user data

--- backend/index.ts
+++ backend/index.ts
@@ -6,22 +6,87 @@ dotenv.config()
 
 const app: Express = express()
 
+app.use(express.json())
 app.use(cors())
+
+const requestLogger = (request, response, next) => {
+	console.log('Method', request.method)
+	console.log('Path', request.path)
+	console.log('Body', request.body)
+	next()
+}
+
+app.use(requestLogger)
 
 //Single supabase client for interacting with database
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
 
 //Routes
+const API_BASE = '/api/budgets'
+
 app.get('/', (req: Request, res: Response) => {
     res.send('<h1>Express installed</h1>')
 })
 
-app.get('/api/budgets', async (req: Request, res: Response) => {
+app.get(API_BASE, async (req: Request, res: Response) => {
     const { data, error } = await supabase
         .from('budgets')
         .select()
 
     res.json(data)
+})
+
+app.get(`${API_BASE}/:userID`, async (req: Request, res: Response) => {
+    const userID = req.params.userID
+
+    const { data, error } = await supabase
+        .from('budgets')
+        .select()
+        .eq('user_id', userID)
+
+    res.json(data)
+})
+
+app.post(API_BASE, async (req: Request, res: Response) => {
+    const newBudget = req.body
+
+    const { data, error } = await supabase
+        .from('budgets')
+        .insert([newBudget])
+
+    console.log('error', error);
+    
+    res.json(data ? data : error)
+})
+
+app.post('/api/user/register', async (req: Request, res: Response) => {
+    const newUser = req.body
+
+    const { user, session, error } = await supabase.auth.signUp(newUser)
+
+    //Create empty budget for new user
+    if (user?.id) {
+        const newBudget = {
+            income: [],
+            outcome: [],
+            balance: 0,
+            user_id: user.id
+        }
+
+        const { data, error } = await supabase
+            .from('budgets')
+            .insert([newBudget])
+    }
+
+    res.json(user?.id ? {id: user.id, email: user.email} : {error: error.message})
+})
+
+app.post('/api/user/login', async (req: Request, res: Response) => {
+    const userInfo = req.body
+
+    const { user, session, error } = await supabase.auth.signIn(userInfo)
+
+    res.json(user?.id ? {id: user.id, email: user.email} : {error: error.message})
 })
 
 //Unknown endpoints
