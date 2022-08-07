60e64d9c43718f11102156cc719f1d5a9a0a3b9d
Facundo Azcue <fazcue@gmail.com>
2022-08-07 04:30:29 -0300

    Updating Login component
    Added Login.module.css with some styles
    Added option to change from 'login' to 'register' to 'recover'
    Update setUser prop definition (was using wrong interface)

--- frontend/components/Login.tsx
+++ frontend/components/Login.tsx
@@ -1,13 +1,16 @@
-import { useState } from 'react'
+import styles from './Login.module.css'
+import { useState, useEffect } from 'react'
 import userService from '../services/user'
-import { UserData } from '../common/types'
+import { User, UserData } from '../common/types'
 
 interface Props {
-    setUser: (value: UserData) => void
+    setUser: (value: User) => void
     setErrorMessage: (value: string) => void
 }
 
 const Login = ({setUser, setErrorMessage}: Props): JSX.Element => {
+    const [selectedOption, setSelectedOption] = useState<'login' | 'register' | 'recover'>('login')
+
     const [userData, setUserData] = useState<UserData>({
         email: '',
         password: ''
@@ -35,7 +38,9 @@ const Login = ({setUser, setErrorMessage}: Props): JSX.Element => {
         }
     }
 
-    const handleRegister = async () => {
+    const handleRegister = async (e) => {
+        e.preventDefault()
+
         if (userData.email && userData.password) {
             const res = await userService.register(userData)
 
@@ -46,6 +51,56 @@ const Login = ({setUser, setErrorMessage}: Props): JSX.Element => {
         }
     }
 
+    const handleRecover = async (e) => {
+        e.preventDefault()
+
+        if (userData.email) {
+            const res = await userService.recover(userData.email)
+
+            if (res.data.error) {
+                setErrorMessage(res.data.error)
+            }
+
+            console.log('recover', res.data)
+        }
+    }
+
+    useEffect(() => {
+        setErrorMessage(null)
+    },[selectedOption])
+
+    if (selectedOption === 'register') {
+        return (
+            <>
+                <h2>Register</h2>
+                <form onSubmit={handleRegister}>
+                    <input required type='email' placeholder='email' value={userData.email} onChange={(e) => handleUserData('email', e.target.value)} />
+
+                    <input required placeholder='password' value={userData.password} type='password' onChange={(e) => handleUserData('password', e.target.value)} />
+
+                    <input type='submit' value='Register' className={styles.submit} />
+
+                    <p className={styles.extraOption} onClick={() => setSelectedOption('login')}>Already have an account? <span className={styles.bold}>Login</span></p>
+                </form>
+            </>
+        )
+    }
+
+    if (selectedOption === 'recover') {
+        return (
+            <>
+                <h2>Recover password</h2>
+                <form onSubmit={handleRecover}>
+                    <input required type='email' placeholder='email' value={userData.email} onChange={(e) => handleUserData('email', e.target.value)} disabled />
+
+                    <input type='submit' value='disabled' className={styles.submit} disabled />
+
+                    <p className={styles.extraOption} onClick={() => setSelectedOption('login')}>Go back to <span className={styles.bold}>Login</span></p>
+                </form>
+            </>
+        )
+    }
+
     return (
         <>
             <h2>Login</h2>
@@ -54,7 +109,11 @@ const Login = ({setUser, setErrorMessage}: Props): JSX.Element => {
 
                 <input required placeholder='password' value={userData.password} type='password' onChange={(e) => handleUserData('password', e.target.value)} />
 
-                <button type='submit'>Log In</button>
+                <input type='submit' value='Log in' className={styles.submit} />
+
+                <p className={styles.extraOption} onClick={() => setSelectedOption('register')}>Don`t have an account yet? <span className={styles.bold}>Register</span></p>
+
+                <p className={styles.extraOption} onClick={() => setSelectedOption('recover')}>Forgot your password? <span className={styles.bold}>Recover it</span></p>
             </form>
         </>
     )
