826a10e8ec26fb82fb3a3c2de2306af52128c92f
Facundo Azcue <fazcue@gmail.com>
2022-08-07 06:59:02 -0300

    Adding width & maxWidth as optional props

--- frontend/components/Card.tsx
+++ frontend/components/Card.tsx
@@ -2,11 +2,13 @@ import styles from './Card.module.css'
 
 interface Props {
     children: JSX.Element
+    width?: string
+    maxWidth?: string
 }
 
-const Card = ({ children }: Props): JSX.Element => {
+const Card = ({ children, width = null, maxWidth = null }: Props): JSX.Element => {
     return (
-        <div className={styles.container}>
+        <div className={styles.container} style={{width: width, maxWidth: maxWidth}}>
             {children}
         </div>
     )
