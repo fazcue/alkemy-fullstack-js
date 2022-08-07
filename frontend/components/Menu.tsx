7a3c437f29fd530be78aab8efd69b72ad9e6fbef
Facundo Azcue <fazcue@gmail.com>
2022-08-07 07:11:25 -0300

    Updating Menu component
    Added handleLogOut as optional prop
    Added Items as local component
    Added logOutButton styles
    Changed @media max-width from 450 to 500 px

--- frontend/components/Menu.tsx
+++ frontend/components/Menu.tsx
@@ -6,35 +6,52 @@ import NewEntry from './NewEntry'
 import Logo from './Logo'
 
 interface Props {
-	budget: Budget
-	setBudget: (value: Budget) => void
-	balance: number
-	userID: string
+	budget?: Budget
+	setBudget?: (value: Budget) => void
+	balance?: number
+	userID?: string
+	handleLogOut?: () => void
 }
 
-const Menu = ({ budget, setBudget, balance = 0, userID }: Props): JSX.Element => {
+const Menu = ({ budget, setBudget, balance = 0, userID = null, handleLogOut }: Props): JSX.Element => {
 	const [modalStatus, setModalStatus] = useState<boolean>(false)
 
 	const toggleModal = () => {
 		setModalStatus(prev => !prev)
 	}
 
+	const Items = () => {
+		return (
+			<div className={styles.items}>
+				<div className={styles.balance} style={{width: !userID ? '100%' : null}}>
+					<span className={styles.balanceTitle}>balance: </span>
+
+					<span style={{color: balance === 0 ? 'var(--almost-dark)' : balance > 0 ? 'var(--dark-cyan)' : 'var(--crimson)', fontWeight: 'bold'}}>${balance}</span>
+				</div>
+
+				{userID &&
+					<>
+						<span className={styles.addNewButton} onClick={toggleModal}>Add new</span>
+
+						<span className={styles.logOutButton} onClick={handleLogOut}>&#10132;</span>
+					</>
+				}
+			</div>
+		)
+	}
+
 	return (
 		<nav className={styles.container}>
 			<Logo />
-			<div className={styles.items}>
-				<div className={styles.balance}>
-					<span className={styles.balanceTitle}>balance: </span>
-					<span style={{color: balance === 0 ? 'var(--almost-dark)' : balance > 0 ? 'var(--dark-cyan)' : 'var(--crimson)', fontWeight: 'bold'}}>${balance}</span>
-				</div>
-				{userID && <span className={styles.addNewButton} onClick={toggleModal}>Add new</span>}
-			</div>
-			{userID && <ModalBox
-				modalStatus={modalStatus}
-				setModalStatus={setModalStatus}
-			>
-				<NewEntry budget={budget} setBudget={setBudget} userID={userID} isModal setModalStatus={setModalStatus} />
-			</ModalBox>}
+			<Items />
+			{userID && 
+				<ModalBox
+					modalStatus={modalStatus}
+					setModalStatus={setModalStatus}
+				>
+					<NewEntry budget={budget} setBudget={setBudget} userID={userID} isModal setModalStatus={setModalStatus} />
+				</ModalBox>
+			}
 		</nav>
 	)
 }
